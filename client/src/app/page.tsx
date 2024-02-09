3dd7abb8625bdbc2631e553972b0f5f322092107
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-10 00:42:56 +0530

    ui

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,24 +1,33 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LineSalesChart from "@/components/visualizations/LineSalesChart";
 import HomeCardData from "@/components/visualizations/HomeCardData";
+import { Card } from "@/components/ui/card";
 import {
-  Card,
-} from "@/components/ui/card";
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { DataTable } from "@/components/tables/jobTableData";
 import { jobColumn, JobColumnType } from "@/components/tables/jobColumn";
 
-async function getJobData():Promise<JobColumnType[]> {
-  const response = await fetch("http://localhost:5000/api/v1/job", { next: { revalidate: 3600 } });
+async function getJobData(): Promise<JobColumnType[]> {
+  const response = await fetch("http://localhost:5000/api/v1/job", {
+    next: { revalidate: 300 },
+  });
 
   if (!response.ok) {
     throw new Error("failed to fetch data");
   }
-  return response.json()
+  return response.json();
 }
-
 
 export default async function Home() {
   const jobData = await getJobData();
+  const latestJobs = jobData.slice(0, 3)
   return (
     <main>
       <Tabs defaultValue="job">
@@ -31,6 +40,32 @@ export default async function Home() {
 
         <TabsContent value="dash">
           <HomeCardData />
+
+          <section className="w-[500px] mx-auto my-10">
+            <Table>
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Invoice</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Method</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              {latestJobs.map((job, id) => (
+                <TableBody>
+                  <TableRow key={id}>
+                    <TableCell className="font-medium">
+                      {job.jobId}
+                    </TableCell>
+                    <TableCell>{job.status}</TableCell>
+                    <TableCell>{job.jobError}</TableCell>
+                    <TableCell className="text-right">{job.customer.cusName}</TableCell>
+                  </TableRow>
+                </TableBody>
+              ))}
+            </Table>
+          </section>
 
           <LineSalesChart />
         </TabsContent>
