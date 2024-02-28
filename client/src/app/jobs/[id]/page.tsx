7abf415d1001746598c22498d0c18f070ec1fb67
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-28 10:50:58 +0530

    fetchh

--- client/src/app/jobs/[id]/page.tsx
+++ client/src/app/jobs/[id]/page.tsx
@@ -7,16 +7,16 @@ type Params = {
 };
 
 export default async function ViewDetailedJob({ params: { id } }: Params) {
-
   async function getJobData() {
     const response = await fetch(`http://localhost:5000/api/v1/job/${id}`, {
       cache: "no-store",
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
+      toast.error("Failed to fetch data");
     }
-    return response.json();
+    const jobData = await response.json();
+    return jobData.job;
   }
 
   const jobData = await getJobData();
@@ -24,7 +24,7 @@ export default async function ViewDetailedJob({ params: { id } }: Params) {
   return (
     <div>
       <h1>Hello, there viewers!</h1>
-      <h2>jobError: {jobData.job.jobError}</h2>
+      <h2>jobError: {jobData.jobError}</h2>
     </div>
   );
 }
